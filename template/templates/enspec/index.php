fc76295dfd42fedfa9b86aad3f4c3b203896e521
Darren Murray <dmurray@ccistudios.com>
2011-05-26 15:28:29 -0400

    Initial stages of site build

---
+++ template/templates/enspec/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+	<jdoc:include type="head" />
+	<link rel="shortcut icon" href="/templates/enspec/images/favicon.ico" />
+	<link rel="stylesheet" href="/templates/enspec/css/template.css" />
+</head>
+
+<body>
+	<div id="top"><div>
+		<div id="mainmenu"><div>
+			<jdoc:include type="modules" name="top" style="xhtml" />
+			<div class="clear"></div>
+		</div></div>
+	</div></div>
+	
+	<div id="bottom"><div>
+		<div>
+			<jdoc:include type="modules" name="bottom" style="xhtml" />
+			<div class="clear"></div>
+		</div>
+		<div>
+			<div class="left">&copy; <font class="purple">EN</font><font class="red">SPEC</font> Inc. <?php echo date ('Y')?>. All Rights Reserved.</div>
+			<div class="right">Site by<a href="http://www.ccistudios.com" target="_blank"> CCI Studios</a></div>
+		</div>
+	</div></div>
+</body>
+</html>
