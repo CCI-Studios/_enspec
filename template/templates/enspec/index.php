d9801a2f3b1d080250f5b3d33926f01a5eb0a28f
Darren Murray <dmurray@ccistudios.com>
2011-05-30 16:55:56 -0400

    initial site build

--- template/templates/enspec/index.php
+++ template/templates/enspec/index.php
@@ -11,19 +11,43 @@
 	<div id="top"><div>
 		<div id="mainmenu"><div>
 			<jdoc:include type="modules" name="top" style="xhtml" />
+		</div></div>
+	</div><div class="clear"></div></div>
+	
+	<div id="wrapper"><div>
+		<div id="header">
+			
+		</div>
+		<div id="content">
+			<div id="component">
+				<jdoc:include type="component">		
+			</div>
+			<div id="bottom">
+			
+			</div>
+		</div>
+		<div id="sidebar">
+			<jdoc:include type="modules" name="sidebar" style="xhtml" />	
+		</div>
+	</div><div class="clear"></div></div>
+	
+	<div id="footer"><div>
+		<div>
+			<jdoc:include type="modules" name="footer" style="xhtml" />
+			<div>
 			<div class="clear"></div>
 		</div></div>
-	</div></div>
-	
-	<div id="bottom"><div>
-		<div>
-			<jdoc:include type="modules" name="bottom" style="xhtml" />
-			<div class="clear"></div>
-		</div>
 		<div>
 			<div class="left">&copy; <font class="purple">EN</font><font class="red">SPEC</font> Inc. <?php echo date ('Y')?>. All Rights Reserved.</div>
 			<div class="right">Site by<a href="http://www.ccistudios.com" target="_blank"> CCI Studios</a></div>
 		</div>
 	</div></div>
+	
+	<div class="hidden">
+		<jdoc:include type="modules" name="hidden" style="xhtml" />
+		<div class="clear"></div>
+	</div>
+	
 </body>
 </html>
+	
