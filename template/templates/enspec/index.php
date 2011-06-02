ff5e8acc7985e339e23d8d2a23536359aae05560
Darren Murray <dmurray@ccistudios.com>
2011-06-02 16:51:12 -0400

    added php for displaying home page

--- template/templates/enspec/index.php
+++ template/templates/enspec/index.php
@@ -1,5 +1,11 @@
 <!DOCTYPE html>
-
+<?php
+$menu = JSite::getMenu();
+if ($menu)
+    $menu = $menu->getActive();
+if ($menu)
+    $menu = $menu->alias;
+?>
 <html>
 <head>
 	<jdoc:include type="head" />
@@ -7,43 +13,53 @@
 	<link rel="stylesheet" href="/templates/enspec/css/template.css" />
 </head>
 
-<body>
+<body class="<?php echo $menu; ?>">
 	<div id="top"><div>
 		<jdoc:include type="modules" name="top" style="xhtml" />
 		<div class="clear"></div>
 	</div></div>
 	
 	<div id="wrapper"><div>
+		<?php if($this->countModules("header")):?>
+		<div id="header">
+			<jdoc:include type="modules" name="header" style="xhtml" />
+			<div class="clear"></div>
+		</div>	
+		<?php endif; ?>
+		
 		<div id="content"><div>
 			<div id="component">
 				<jdoc:include type="component" />
 				<div class="clear"></div>
-			</div>	
+			</div>
+			
 			<div id="sidebar">
 				<jdoc:include type="modules" name="sidebar" style="xhtml" />
 				<div class="clear"></div>
+			</div>	
+		<?php if($this->countModules("bottom")):?>
+			<div id="bottom">
+				<jdoc:include type="modules" name="bottom" style="xhtml" />
+				<div class="clear"></div>
 			</div>
-			
-			<div class="clear"></div>			
-		</div></div>		
-	</div></div>
-	
-	<div id="footer"><div>
-		<div>
-			<jdoc:include type="modules" name="footer" style="xhtml" />
+		<?php endif; ?>	
+		</div></div>
+		
+		<div id="bottom_menu">
+			<jdoc:include type="modules" name="bottom_menu" style="xhtml" />
 			<div class="clear"></div>
 		</div>
-		<div>
-			<div class="left">&copy; <font class="purple">EN</font><font class="red">SPEC</font> Inc. <?php echo date ('Y')?>. All Rights Reserved.</div>
+		
+		<div id="footer"><div>
+			<div class="left">&copy; <span class="purple">EN</span><span class="red">SPEC</span> Inc. <?php echo date ('Y')?>. All Rights Reserved.</div>
 			<div class="right">Site by<a href="http://www.ccistudios.com" target="_blank"> CCI Studios</a></div>
+		</div></div>		
+
+		<div class="hidden">
+			<jdoc:include type="modules" name="hidden" style="xhtml" />
+			<div class="clear"></div>
 		</div>
 	</div></div>
-	
-	<div class="hidden">
-		<jdoc:include type="modules" name="hidden" style="xhtml" />
-		<div class="clear"></div>
-	</div>
-	
 </body>
 </html>
 	
